06cd01d2e1737ec3b6a78198d33c2fbfb127a92e
Keever <akeever1122@gmail.com>
2024-10-17 13:01:49 -0500

    Updates data to include more years, and updates model to get rid of neonate survival and productivity and instead use recruitment rate to simplify the model.

--- Rscripts/Functions_AbundanceEstimation.R
+++ Rscripts/Functions_AbundanceEstimation.R
@@ -6,27 +6,9 @@
 
 #   Allison C Keever
 #   TNTech University
-#   Last updated: 5/25/23
+#   Last updated: 10/17/24
 
 ##############################################################################L
-
-
-# Function to run model in JAGS. Useful if running many different models with 
-# different data
-run_model <- function(script, ni, nb, nt, nc, win.data, params, inits) {
-  # Run the model
-  out <- jags(win.data, inits, params, script, 
-              n.chains = nc, n.thin = nt, n.iter = ni, n.burnin = nb, 
-              jags.module = c("glm", "dic"))
-  
-  if(any(out$BUGSoutput$summary[,"Rhat"] > 1.05)){
-    out <- autojags(out, n.iter = 200000, n.thin = nt, n.update = 5)
-  }
-  
-  return(out)
-  
-}
-
 
 
 # Function to estimate/calculate initial population size for deer based on 
@@ -47,34 +29,45 @@ est_init_pop <- function(y.H, y.Af, y.Am, harvest.rate = c(0.09, 0.20),
           arrange(desc(bag)) %>%
           split(.$bag) %>%
           map(~ .x %>% select(-bag)))
-    
-
-  yearly_props <- list("antlered" =
-    data.frame("antlered" = apply(abind(array_tree(y.Af, margin = 3) %>% 
-                                          replace(0, 1) %>%
-                                          map(~ as.data.frame(.x) %>% 
-                                              mutate(across(where(is.numeric), 
-                                                            prop.table))), 
-                                      along = 3), 1, mean)), 
-    "antlerless" =
-    data.frame("antlerless" = apply(abind(array_tree(y.Af, margin = 3) %>% 
-                                            replace(0, 1) %>%
-                                            map(~ as.data.frame(.x) %>% 
-                                              mutate(across(where(is.numeric), 
-                                                            prop.table))), 
-                                      along = 3), 1, mean)))
-
+  
+  
+  yearly_props <- list(
+    "antlered" = data.frame(
+      "antlered" = apply(abind(array_tree(y.Am, margin = 3) %>% 
+                                 replace(0, 1) %>%
+                                 map(~ as.data.frame(.x) %>% 
+                                       mutate(across(where(is.numeric), 
+                                                     prop.table))), 
+                               along = 3), 1, mean)), 
+    "antlerless" = data.frame(
+      "antlerless" = apply(abind(array_tree(y.Af, margin = 3) %>% 
+                                   replace(0, 1) %>%
+                                   map(~ as.data.frame(.x) %>% 
+                                         mutate(across(where(is.numeric), 
+                                                       prop.table))), 
+                                 along = 3), 1, mean)))
+  
   
   init_pop <- abind::abind(map(est_harvest, 
-                  ~ as.data.frame(do.call(cbind, map2(.x, yearly_props, 
-                                        ~ round(mapply(`*`, .y, .x), 0)))) %>%
-                    select("antlerless", "antlered")), along = 3)
+                               ~ as.data.frame(
+                                 do.call(
+                                   cbind,
+                                   map2(.x, 
+                                        yearly_props,
+                                        ~ round(mapply(`*`, .y, .x), 0)) %>%
+                                     assign_in(list(1), 
+                                               rbind(pluck(., 2, 4), 
+                                                     pluck(., 1)))  %>% 
+                                     assign_in(list(2), 
+                                               t(rbind(pluck(., 2)[-4,]))))) %>% 
+                                 rename("antlerless" = V2) %>%
+                                 select("antlerless", "antlered")), along = 3)
   
   for(dmu in 1:dim(init_pop)[3]){
     init_pop[1,,dmu] <- round(init_pop[2,,dmu] / (0.6 * (1 - harvest.rate)), 0)
   }
   
-  init_pop <- init_pop
+  init_pop <- init_pop * 0.88
   
   return(init_pop)
   
@@ -83,8 +76,8 @@ est_init_pop <- function(y.H, y.Af, y.Am, harvest.rate = c(0.09, 0.20),
 
 # Function to estimate/calculate initial population size for deer based on 
 # harvest data (total and sexed/aged data), reporting rate, and harvest rate. 
-init_pop_values <- function(y.H, y.Af, y.Am, harvest.rate = c(0.09, 0.20), 
-                         report.rate = c(0.8, 0.76)){
+init_pop_values <- function(y.H, y.Af, y.Am, harvest.rate = c(0.15, 0.30), 
+                            report.rate = c(0.82, 0.78)){
   y.H <- array_tree(y.H, margin = 3)
   
   est_harvest <- y.H %>% 
@@ -96,27 +89,38 @@ init_pop_values <- function(y.H, y.Af, y.Am, harvest.rate = c(0.09, 0.20),
   
   
   yearly_props <- list("antlered" =
-                         data.frame("antlered" = apply(abind(array_tree(y.Af, margin = 3) %>% 
-                                                               replace(0, 1) %>%
-                                                               map(~ as.data.frame(.x) %>% 
-                                                                     mutate(across(where(is.numeric), 
-                                                                                   prop.table))), 
-                                                             along = 3), 1, mean)), 
+                         data.frame("antlered" = 
+                                      apply(abind(
+                                        array_tree(y.Am, margin = 3) %>% 
+                                          replace(0, 1) %>%
+                                          map(~ as.data.frame(.x) %>% 
+                                                mutate(across(where(is.numeric), 
+                                                              prop.table))), 
+                                        along = 3), 1, mean)), 
                        "antlerless" =
-                         data.frame("antlerless" = apply(abind(array_tree(y.Af, margin = 3) %>% 
-                                                                 replace(0, 1) %>%
-                                                                 map(~ as.data.frame(.x) %>% 
-                                                                       mutate(across(where(is.numeric), 
-                                                                                     prop.table))), 
-                                                               along = 3), 1, mean)))
+                         data.frame("antlerless" = 
+                                      apply(abind(
+                                        array_tree(y.Af, margin = 3) %>% 
+                                          replace(0, 1) %>%
+                                          map(~ as.data.frame(.x) %>% 
+                                                mutate(across(where(is.numeric), 
+                                                              prop.table))), 
+                                        along = 3), 1, mean)))
   
-
+  
   init_pop <- abind::abind(
     map(est_harvest, 
-        ~ aperm(apply(abind::abind(map2(.x, yearly_props, 
-                            ~ round(mapply(`*`, .y, .x, USE.NAMES = FALSE), 0)), 
-                       along = 3), 1:2, function(x) sort(x, decreasing = TRUE)), c(2,3,1))),
-          along = 4)
+        ~ aperm(apply(abind::abind(
+          map2(.x, 
+               yearly_props,
+               ~ round(mapply(`*`, .y, .x, USE.NAMES = FALSE), 0)) %>%
+            assign_in(list(1), 
+                      rbind(pluck(., 2)[4,], 
+                            pluck(., 1)))  %>% 
+            assign_in(list(2), 
+                      rbind(pluck(., 2)[-4,])),
+          along = 3), 1:2, function(x) sort(x, decreasing = TRUE)), c(2,3,1))),
+    along = 4)
   
   for(dmu in 1:dim(init_pop)[4]){
     for(bag in 1:dim(init_pop)[3]){
@@ -133,47 +137,3 @@ init_pop_values <- function(y.H, y.Af, y.Am, harvest.rate = c(0.09, 0.20),
   
 }
 
-
-# Wrapper function to generate priors for survival, productivity, and reporting 
-# rate.
-generate_priors <- function(values, dist.type, ...) {
-  
-  if(dist.type == "beta") {
-    rates <- t(apply(values, 1, 
-                     FUN = function(x) unlist(beta.MoM.fcn(mean = x[1], 
-                                                           var = x[2]))))
-    
-  } else if(dist.type == "gamma") {
-    rates <- t(apply(values, 1, 
-                     FUN = function(x) unlist(gamma.MoM.fcn(mean = x[1], 
-                                                            var = x[2]))))
-  } else {
-    warning("Please provide dist.type of either 'beta', or 'gamma'")
-  }
-  
-  return(rates)
-}
-
-
-# MoM for beta distribution
-beta.MoM.fcn <- function (mean, var, plot = FALSE){
-  alpha <-mean * ((mean * (1 - mean) / var) - 1)
-  beta <-(1 - mean) * ((mean * (1 - mean) / var) - 1)
-  y <-rbeta(1:1000, alpha, beta)
-  if(plot == TRUE){
-    beta.plot <-plot(y, dbeta(y, alpha, beta), ylab = "Frequency", xlab = "Rate")
-  }
-  return(c(list(alpha = alpha, beta = beta)))
-}
-
-
-# MoM for gamma distribution
-gamma.MoM.fcn <- function(mean, var, plot = FALSE) {
-  shape <- (mean ^ 2) / var 
-  rate <- mean / var
-  y <- rgamma(1:1000, shape = shape, rate = rate)
-  if(plot == TRUE){
-    gamma.plot <- plot(y, dgamma(y, shape, rate), ylab = "Frequency", xlab = "Rate")
-  }
-  return(c(list(shape = shape, rate = rate)))
-}
--- Rscripts/TN_harvest_data_prep.R
+++ Rscripts/TN_harvest_data_prep.R
@@ -3,7 +3,7 @@
 #   TN Abundance Estimation Data Prep
 #       
 #       Running this script will take supplied harvest data (Data/dkmaster.RData),
-#       estiamted harvest from social surveys (Data/HarvestEstimates.csv), and 
+#       estimated harvest from social surveys (Data/HarvestEstimates.csv), and 
 #       DMU designations (Data/dmus.csv) from TN and prep it for the 
 #       Abundance Estimation Analysis (TN_AbundanceEstimation). 
 #       
@@ -11,12 +11,12 @@
 #           1. Harvest data statewide (y_H_statewide) or by DMU (y_H_DMU)
 #           2. Aged and sexed harvest statewide (y_Af_statewide - females; 
 #               y_Am_statewide - males) or by DMU (y_Af_DMU and y_Am_DMU)
-#           3. Estimated harvest from social surveys statewide (h.est) and by 
-#               DMU (h.est.dmu)
+#           3. Estimated harvest from social surveys (statewide (h.est) and by 
+#               DMU (h.est.dmu))
 #   
 #   Allison C Keever
 #   TNTech University
-#   Last Updated: 5/25/23
+#   Last Updated: 10/19/24
 
 ###############################################################################L
 
@@ -34,15 +34,12 @@ harvest_ests <- read.csv("Data/HarvestEstimates.csv")
 # Data manipulation and prep----------------------------------------------------
 
 # Only keep the variables we will use, and get the DMU based on county
-tn_harvest_data <- dkmaster %>%
-  filter(!(huntyear %in% c(2020, 2021, 2022))) %>%
+tn_harvest_data <- harvest_datum %>%
   select(county, age, sex, huntyear, bag) %>%
-  left_join(dmus %>%
-              select(NAME, alias, pool), 
-            by = c("county" = "NAME"))
+  left_join(dmus, 
+            by = c("county" = "County"))
 
-# Fix issues in harvest data to keep nomenclature consistent. Also define age
-# and sex classes. 
+# Fix issues in harvest data to keep nomenclature consistent
 tn_harvest_data <- tn_harvest_data %>%
   mutate(age = as.numeric(case_when(is.na(age) | age == "none" ~ "NA",
                                     age == "0.5" ~ "1",
@@ -53,8 +50,7 @@ tn_harvest_data <- tn_harvest_data %>%
                                     sex == "male" ~ "2")))
 
 # Get sex/aged data statewide and by DMU for males (Am) and females (Af)...
-# y_sex_statewide[age, yr]
-# y_sex_DMU[age, yr, DMU]
+# y_sex_scale[age, yr]
 y_Af_statewide <- tn_harvest_data %>% 
   filter(!is.na(age), bag == "antlerless", sex == 1 | (sex == 2 & age == 1)) %>%
   group_by(huntyear, age, sex) %>%
@@ -76,30 +72,29 @@ y_Am_statewide <- tn_harvest_data %>%
 
 y_Af_DMU <- tn_harvest_data %>% 
   filter(!is.na(age), bag == "antlerless", sex == 1 | (sex == 2 & age == 1)) %>%
-  group_by(huntyear, age, sex, alias) %>%
+  group_by(huntyear, age, sex, DMU) %>%
   summarise(H.total = n()) %>%
   pivot_wider(names_from = huntyear, names_prefix = "yr", values_from = H.total, 
               values_fill = 0) %>%
   ungroup() %>%
-  split(.$alias) %>%
-  map(~ .x %>% arrange(sex, age) %>% select(-age, -sex, -alias)) %>%
+  split(.$DMU) %>%
+  map(~ .x %>% arrange(sex, age) %>% select(-age, -sex, -DMU)) %>%
   abind::abind(., along = 3)
 
 y_Am_DMU <- tn_harvest_data %>% 
   filter(!is.na(age), bag == "antlered", sex == 2, age %in% c(2,3)) %>%
-  group_by(huntyear, age, sex, alias) %>%
+  group_by(huntyear, age, sex, DMU) %>%
   summarise(H.total = n()) %>%
   pivot_wider(names_from = huntyear, names_prefix = "yr", values_from = H.total, 
               values_fill = 0) %>%
   ungroup() %>%
-  split(.$alias) %>%
-  map(~ .x %>% select(-age, -sex, -alias)) %>%
+  split(.$DMU) %>%
+  map(~ .x %>% select(-age, -sex, -DMU)) %>%
   abind::abind(., along = 3)
 
-# Get total harvest statewide and by DMU
-# y_H_statewide[bag, yr]
-# y_H_DMU[bag, yr, DMU]
+# Get total harvest statewide and by DMU... y_H_scale[bag, yr]
 y_H_statewide <- tn_harvest_data %>%
+  filter(bag != "not reported") %>% 
   group_by(huntyear, bag) %>%
   summarise(H.total = n()) %>%
   arrange(desc(bag)) %>%
@@ -108,25 +103,24 @@ y_H_statewide <- tn_harvest_data %>%
   select(-huntyear)
 
 y_H_DMU <- tn_harvest_data %>%
-  filter(!is.na(alias)) %>%
-  group_by(huntyear, bag, alias) %>%
+  filter(!is.na(DMU), bag != "not reported") %>%
+  group_by(huntyear, bag, DMU) %>%
   summarise(H.total = n()) %>%
   arrange(desc(bag)) %>%
   pivot_wider(names_from = bag, values_from = H.total, values_fill = 0) %>%
   ungroup() %>%
-  split(.$alias) %>%
-  map(~ .x %>% select(-huntyear, -alias)) %>%
+  split(.$DMU) %>%
+  map(~ .x %>% select(-huntyear, -DMU)) %>%
   abind::abind(., along = 3)
 
 
-# Get total number of males and females that were aged and sexed by DMU
+# Get total number of males and females that were aged and sexed
 O_dmu <- as.data.frame(apply(y_Af_DMU, c(2, 3), sum)) %>% 
   rownames_to_column(var = "year") %>% 
   pivot_longer(cols = !contains("year"), names_to = "DMU", values_to = "fN") %>% 
   full_join(as.data.frame(apply(y_Am_DMU, c(2, 3), sum)) %>% 
               rownames_to_column(var = "year") %>% 
-              pivot_longer(cols = !contains("year"), names_to = "DMU", 
-                           values_to = "mN")) %>% 
+              pivot_longer(cols = !contains("year"), names_to = "DMU", values_to = "mN")) %>% 
   split(.$DMU) %>% 
   map(~ .x %>% select(- year, - DMU)) %>% 
   abind::abind(., along = 3)
@@ -135,20 +129,18 @@ O_dmu <- as.data.frame(apply(y_Af_DMU, c(2, 3), sum)) %>%
 # Estimated harvest statewide
 h.est <- harvest_ests %>% 
   filter(DMU == "State") %>% 
-  arrange(Year) %>% 
+  arrange(year) %>% 
   mutate(b = ifelse(Bag == "Antlerless", 1, 2),
-         SD_harv_est = ifelse(is.na(SD_harv_est), 
-                              mean(.$SD_harv_est, na.rm = TRUE), 
+         SD_harv_est = ifelse(is.na(SD_harv_est), mean(.$SD_harv_est, na.rm = TRUE), 
                               SD_harv_est)) %>%
   split(.$b) %>%
   map(~ .x %>% select(Harv_est, SD_harv_est)) %>%
   abind::abind(., along = 3)
 
-
 # Estimated harvest by DMU
 h.est.dmu <- array(unlist(harvest_ests %>% 
                             filter(DMU != "State") %>%
-                            arrange(Year) %>% 
+                            arrange(year) %>% 
                             mutate(b = ifelse(Bag == "Antlerless", 1, 2),
                                    SD_harv_est = ifelse(is.na(SD_harv_est), 
                                                         mean(.$SD_harv_est, 
@@ -156,12 +148,11 @@ h.est.dmu <- array(unlist(harvest_ests %>%
                                                         SD_harv_est)) %>%
                             split(.$DMU) %>%
                             map(~ .x %>% split(.$b)) %>%
-                            map(~ .x %>% map(~ .x %>% 
-                                               select(Harv_est, SD_harv_est)))), 
-                   dim = c(length(unique(harvest_ests$Year)), 2, 2, 
+                            map(~ .x %>% map(~ .x %>% select(Harv_est, SD_harv_est)))), 
+                   dim = c(length(unique(harvest_ests$year)), 2, 2, 
                            length(unique(harvest_ests$DMU)) - 1))
 
 
 # Get rid of stuff we don't need clogging up our environment
-rm(dmus, dkmaster, tn_harvest_data, harvest_ests)
+rm(dmus, harvest_datum, tn_harvest_data, harvest_ests)
 
